d996dcdbaacde157a5a0575c18cbefbaa15d266c
andreskalel <andres@dreamsengine.io>
2022-03-23 12:46:11 -0600

    Add nuext 3

---
+++ pages/about.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+
+const counter = useState('counter', () => Math.round(Math.random() * 1000))
+const { data } = await useAsyncData('count', () => $fetch('https://randomuser.me/api/'));
+const first = data.value.results[0].name.first;
+useMeta({
+      title: "Acerca de :: Nuxt " + first,
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' }
+      ],
+      bodyAttrs: {
+        class: 'test'
+      }
+})
+
+</script>
+
+<template>
+  <div>
+    Counter: {{ counter }}
+    <button @click="counter++">
+      +
+    </button>
+    <button @click="counter--">
+      -
+    </button>
+  </div>
+  <p>{{ first }}</p>
+</template>
